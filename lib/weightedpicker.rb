62a813f0c6cb6397838b32838ca15d4ffdeb3ad6
ippei94da <ippei94da@gmail.com>
2012-04-13 14:24:52 +0900

    Translate rspec

---
+++ lib/weightedpicker.rb
@@ -0,0 +1,233 @@
+require 'yaml'
+
+# TODO
+# 	initialize.指定したファイル内のデータが WeightedPicker 的に
+# 	解釈できなければ例外。
+
+#= 概要
+# 要素群の中から、優先度に応じた重み付きランダムで、
+# どれか1つを選択する。
+# たとえば、以下の用途に使える。
+#* 音楽ファイルの再生(好みのものは多い頻度で、
+#  そうでないものは少ない頻度で)
+#* クイズゲームの問題選択
+#* 画像
+#  * 写真で壁紙, スライドショー的な用途。
+#
+# 対象の要素はファイルであることを前提としない。
+# 文字列であることが多いかもしれない。
+#
+#= 確率として評価を保存
+# 全ての要素の好ましさを点数として記録。
+# 対象要素全てで好ましさを総和したものが母数、
+# それに対する要素の点数/点数の総和 がその要素の選択される確率。
+#
+#= 保存ファイルが必要
+# 保存ファイル名を必ず保持する。
+# 当クラスは、中長期的に使用することで選択を賢くするのが目的。
+# 現在の成績を保存しないということはほとんど考えられない。
+#
+#= 時間を評価関数に入れない
+# 「対象の要素の好ましさの、好ましさの総量に対する割合」が
+# その要素が選ばれる確率となる。
+# 時間を評価に入れると構造が複雑になるし、
+# 機会と時間は一般にそれほどよい相関がない。
+# 毎日使うこともあれば、1ヶ月あくこともある。
+#
+#= 使用 != 鑑賞
+# ライブラリの考え方としては使用=鑑賞とは仮定しない。
+# 要素が pick された瞬間では点数操作は行わず、
+# 明示的に点数操作が指示されたときだけ変化・保存する。
+#
+#= 加点と減点
+# 点数操作が行われる度に規格化する。
+#
+#= 評価の上限と下限
+#== 上限
+# 点数の上限は 1.0。
+# 上限を越える点数の要素が現れたら、
+# それが 1.0 になるように全ての要素の評価を除算して正規化する。
+#
+#== 下限
+# 点数の下限は 2.0 ** (-500) としておく。
+# 下限を下回る点数の要素が現れたら、
+# その要素の点数を下限値に設定する。
+# これが重要になるのはクイズゲームで間違ったときにその問題以外の
+# 評価が相対的に下がる現象。
+# Ruby 1.9.1 において扱える最小の数を確認してみたところ、
+# 2.0 **(-1074) # => 5.0e-324
+# 2.0 **(-1075) # => 0.0
+# となり、無限小の有限の数が扱えず、
+# どこかで潰れて 0.0 になることが分かる。
+# 指数 -500 は有限の数から 0.0 になる境界と
+# 最頻値 1.0 の中間くらいの指数として選ばれた。
+#
+# 1/100万で人間的尺度では無視して良い確率だが、
+# これを大きく下回る確率としている。
+# 普通 500 回も間違えることは考えにくい。
+# そして 500回間違えたとしても、
+# 2.0 **(- 500) 精度で評価が保存可能。
+#
+# ただし、0.0 がセットされた要素はこれを下限値に修正したりはせず、
+# 0.0 のままとする。
+# これは たとえばメイドさんRnR のように自動的には選ばれて
+# 欲しくないものだけど、ファイルを消すのは惜しいものを扱うため。
+# また、既にデータ管理がなされていることを示す必要がある。
+#
+#= 連続選択を制限する方法は導入しない
+# いろいろ複雑になる。
+# とりあえずはこの仕組みを導入しない。
+# これに対応するにはまず history をデータとして残す必要がある。
+# また計算通りの確率から乱れない方法が好ましいが、
+# そのような計算方法はぱっと思い付かない。
+# 
+# たとえば定数回以内の選択を拒否する仕組みでは、
+# 重み 100 が1個(A)と重み1が1個(B)で全2個状態では、
+# 連続を認めない(間に 1つ入れる必要あり)という場合では、
+# 本来 99:1 で実行される筈なのに 1:1 になってしまう。
+# 
+# 間隔の期待値に定数を除算する方法も考えたが、
+# 重み0のものはゼロ割りになるなど扱いが面倒になる。
+# 
+# ヒストリ関係はこのクラスの外に出した方が良いと判断。
+class WeightedPicker
+	MAX_WEIGHT = 1.0
+	MIN_WEIGHT = 2.0 ** (-500)
+
+	class InvalidFilenameError < Exception; end
+	class NoEntryError         < Exception; end
+	class NotExistKeyError     < Exception; end
+
+	# Initialization.
+	# Argument 'file' indicates a strage file name for data
+	# which this class manages.
+	# If the 'file' does not exist, this file is used to data strage.
+	#
+	# Argument 'items' is an array of items to be managed.
+	# Not all the items in the 'file' survive.
+	# A and B in the file and B and C in items,
+	# then B and C is the items which this class manage.
+	# A is discarded from record.
+	def initialize(file, items)
+		@save_file = file
+		unless File.exist? @save_file
+			File.open(@save_file, "w") {|io| YAML.dump({}, io)}
+		end
+		@weights = YAML.load_file(file)
+
+		merge(items)
+		normalize_write
+	end
+
+	#乱数を利用して優先度で重み付けして要素を選び、要素を返す。
+	def pick
+		raise NoEntryError if @weights.empty?
+
+		##乱数を 2つ生成し、adopt? を呼ぶ。
+		##乱数でどれか1つを選ぶ。
+		##その priority に応じて、確率で採用する。
+		while ( true )
+			item = @weights.keys[ rand( @weights.size ) ]
+
+			#next if ( ! pick?( item ) )
+			if adopt?( item, rand )
+				return item
+			end
+		end
+	end
+
+	#重みを重くする。(優先度が上がる)
+	def weigh(item)
+		#pp @weights
+		raise NotExistKeyError unless @weights.has_key?(item)
+		@weights[ item ] *= 2.0
+		normalize_write
+	end
+
+	#重みを軽くする。(優先度が下がる)
+	def lighten( item )
+		raise NotExistKeyError unless @weights.has_key?(item)
+		@weights[ item ] /= 2.0
+		normalize_write
+	end
+
+	##管理している要素の数を返す。
+	#def size
+	#	@weights.size
+	#end
+
+	##管理している要素と重みでイテレート。
+	##e.g. ws0.each { |item, weight| p item, weight }
+	##item が管理しているオブジェクト、weight が重み。
+	#def each
+	#	@weights.each do |item, weight|
+	#		yield( item, weight )
+	#	end
+	#end
+
+	private
+
+	# 引数 keys で示したものと、
+	# 内部的に管理しているデータが整合しているかチェックし、
+	# keys に合わせる。
+	# 追加されたデータは MAX_WEIGHT の重みとなる。
+	# データが削除された場合、それが最大値の可能性があるので
+	# 必ず normalize_write される。
+	def merge(keys)
+		keys.each do |key|
+			@weights[key] = MAX_WEIGHT unless
+				@weights.keys.include?(key)
+		end
+		@weights.each do |key, val|
+			@weights.delete(key) unless keys.include?(key)
+		end
+		normalize_write
+	end
+
+	# given_val は 0.0〜1.0 の間の実数とする。
+	# 重みと 与えられた given_val が等しい場合は false になる。
+	# これは重み 1.0 が稀に採用されないことがあっても
+	# 重み 0.0 が稀にでも採用されることを嫌ってのこと。
+	def adopt?( item, given_val )
+		raise NotExistKeyError unless @weights.has_key?(item)
+
+		return given_val < (@weights[item])
+	end
+
+	##データを更新し、整合性を確保する。
+	#def refresh
+	#	TODO
+	#	raise れいがいめい
+	#	"Cannot do refresh because no entry exists." if @weights.size == 0
+
+	#	#normalize のために事前設定。
+	#	@max_weight = @weights.max{ |a, b| a[1] <=> b[1] }[1]
+	#	normalize
+
+	#	#normalize したあとのデータに従い、total_weight などを算出。
+	#	@total_weight = @weights.values.inject( 0 ){ |sum, i| sum += i }
+	#	@max_weight = @weights.max{ |a, b| a[1] <=> b[1] }[1]
+	#end
+
+	# 最大値を max とするように規格化する。
+	# ただし、weight が MIN_WEIGHT 未満となった項目は
+	# MIN_WEIGHT を新しい weight とする。
+	def normalize_write
+		raise NoEntryError if @weights.size == 0
+
+		old_max = @weights.values.max
+		@weights.each do |key, val|
+			new_val = (val * (MAX_WEIGHT / old_max ) )
+			if new_val < MIN_WEIGHT
+				@weights[key] = MIN_WEIGHT
+			else
+				@weights[key] = new_val
+			end
+		end
+
+		File.open(@save_file, "w") do |io|
+			YAML.dump(@weights, io)
+		end
+	end
+
+end
--- spec/weightedpicker_spec.rb
+++ spec/weightedpicker_spec.rb
@@ -1,7 +1,292 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require "stringio"
+require "fileutils"
 
-describe "Weightedpicker" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+class WeightedPicker
+  attr_accessor :weights
+  public :normalize_write, :adopt?, :merge
+end
+
+TMP_FILE = "tmp.yaml"
+NOT_EXIST_FILE = "not_exist_file"
+
+
+
+describe "Weightedpicker::initialize" do
+  before do
+    weights = { "A" => 1.0, "B" => 0.5, }
+    items = ["A", "B"]
+    File.open(TMP_FILE, "w") { |io| YAML.dump(weights, io) }
+    @wp01 = WeightedPicker.new(TMP_FILE, items)
+    @wp02 = Marshal.load(Marshal.dump(@wp01))
+    @wp02.weights = {}
+    FileUtils.rm NOT_EXIST_FILE if File.exist? NOT_EXIST_FILE
   end
+
+  it "should create new file when the file not exist" do
+    # 指定したファイルが存在しない場合、
+    t = WeightedPicker.new(NOT_EXIST_FILE, ["A","B"])
+    t.weights.should == { "A" => 1.0, "B" => 1.0}
+    YAML.load_file(NOT_EXIST_FILE).should == { "A" => 1.0, "B" => 1.0, }
+  end
+  
+  it "should raise exception" do
+    # 作成できないファイル名。
+    lambda{WeightedPicker.new("", ["A","B"])}.should raise_error(Errno::ENOENT)
+  end
+
+  it "should read correctly" do
+    # 正しく取り込めているか？
+    @wp01.weights.should == { "A" => 1.0, "B" => 0.5, }
+  end
+
+  it "should normalize when the maximum in the file not the same as MAX_WEIGHT" do
+    # 指定したファイル内の重みの最大値が MAX_WEIGHT と異なれば、
+    # 全体を normalize して格納。
+    weights = { "A" => 2.0, "B" => 0.5, }
+    File.open(TMP_FILE, "w") {|io| YAML.dump(weights, io) }
+    WeightedPicker.new(TMP_FILE, ["A","B"]).weights.should == { "A" => 1.0, "B" => 0.25, }
+    #
+    weights = { "A" => 0.25, "B" => 0.5, }
+    File.open(TMP_FILE, "w"){ |io| YAML.dump(weights, io) }
+    WeightedPicker.new(TMP_FILE, ["A","B"]).weights.should == { "A" => 0.5, "B" => 1.0, }
+  end
+
+  it "should merge when keys between weights and items" do
+    # weights と items が異なる場合
+    weights = { "A" => 1.0, "B" => 0.50, }
+    File.open(TMP_FILE, "w") {|io| YAML.dump(weights, io) }
+    WeightedPicker.new(TMP_FILE, ["B","C"]).weights.should == { "B" => 0.5, "C" => 1.0, }
+  end
+
+  after do
+    FileUtils.rm TMP_FILE  if File.exist? TMP_FILE
+    FileUtils.rm NOT_EXIST_FILE if File.exist? NOT_EXIST_FILE
+  end
+
+end
+
+describe "Weightedpicker::merge" do
+  before do
+    weights = { "A" => 1.0, "B" => 0.5, }
+    items = ["A", "B"]
+    File.open(TMP_FILE, "w") { |io| YAML.dump(weights, io) }
+    @wp01 = WeightedPicker.new(TMP_FILE, items)
+
+    @wp02 = Marshal.load(Marshal.dump(@wp01))
+    @wp02.weights = {}
+
+    FileUtils.rm NOT_EXIST_FILE if File.exist? NOT_EXIST_FILE
+  end
+
+  it "when keys given are less than weights file" do
+    # 少ない場合
+    t = Marshal.load(Marshal.dump(@wp01))
+    keys = ["B"]
+    t.merge(keys)
+    t.weights. should == { "B" => 1.0 }
+    # 書き込みチェック
+    YAML.load_file(TMP_FILE).should == { "B" => 1.0 }
+  end
+
+  it "when keys given are more than weights file" do
+    # 多い場合
+    t = Marshal.load(Marshal.dump(@wp01))
+    keys = ["A", "B", "C"]
+    t.merge(keys)
+    t.weights.should == { "A" => 1.0, "B" => 0.5, "C" => 1.0, }
+    # 書き込みチェック
+    YAML.load_file(TMP_FILE).should == { "A" => 1.0, "B" => 0.5, "C" => 1.0}
+  end
+
+  it "when keys given are the same as weights file" do
+    t = Marshal.load(Marshal.dump(@wp01))
+    # 同じ場合
+    keys = ["A", "B"]
+    t.merge(keys)
+    t.weights.should == { "A" => 1.0, "B" => 0.5}
+    # 書き込みチェック
+    YAML.load_file(TMP_FILE).should == { "A" => 1.0, "B" => 0.5}
+  end
+
+  after do
+    FileUtils.rm TMP_FILE  if File.exist? TMP_FILE
+    FileUtils.rm NOT_EXIST_FILE if File.exist? NOT_EXIST_FILE
+  end
+
+end
+
+describe "Weightedpicker::pick" do
+  before do
+    weights = { "A" => 1.0, "B" => 0.5, }
+    items = ["A", "B"]
+    File.open(TMP_FILE, "w") { |io| YAML.dump(weights, io) }
+    @wp01 = WeightedPicker.new(TMP_FILE, items)
+
+    @wp02 = Marshal.load(Marshal.dump(@wp01))
+    @wp02.weights = {}
+
+    FileUtils.rm NOT_EXIST_FILE if File.exist? NOT_EXIST_FILE
+  end
+
+  it "should pick" do
+    ## No test, beccause including randomness.
+    #assert_raise(WeightedPicker::NoEntryError){@wp02.pick}
+    #100.times do 
+    #  assert_equal(true, ["A","B"].include?(@wp01.pick))
+    #end
+  end
+
+  after do
+    FileUtils.rm TMP_FILE  if File.exist? TMP_FILE
+    FileUtils.rm NOT_EXIST_FILE if File.exist? NOT_EXIST_FILE
+  end
+
+end
+
+describe "Weightedpicker::weigh" do
+  before do
+    weights = { "A" => 1.0, "B" => 0.5, }
+    items = ["A", "B"]
+    File.open(TMP_FILE, "w") { |io| YAML.dump(weights, io) }
+    @wp01 = WeightedPicker.new(TMP_FILE, items)
+    @wp02 = Marshal.load(Marshal.dump(@wp01))
+    @wp02.weights = {}
+
+    FileUtils.rm NOT_EXIST_FILE if File.exist? NOT_EXIST_FILE
+  end
+
+  it "should weigh A" do
+    t = Marshal.load(Marshal.dump(@wp01))
+    t.weigh("A")
+    t.weights.should == { "A" => 1.0, "B" => 0.25 }
+    # 書き込みチェック
+    YAML.load_file(TMP_FILE).should == { "A" => 1.0, "B" => 0.25 }
+  end
+
+  it "should weigh B" do
+    t = Marshal.load(Marshal.dump(@wp01))
+    t.weigh("B")
+    t.weights.should == { "A" => 1.0, "B" => 1.0 }
+    # 書き込みチェック
+    YAML.load_file(TMP_FILE).should == { "A" => 1.0, "B" => 1.0 }
+  end
+
+  it "should raise error" do
+    lambda{Marshal.load(Marshal.dump(@wp01)).weigh("C")}.should raise_error(WeightedPicker::NotExistKeyError)
+  end
+
+  after do
+    FileUtils.rm TMP_FILE  if File.exist? TMP_FILE
+    FileUtils.rm NOT_EXIST_FILE if File.exist? NOT_EXIST_FILE
+  end
+
+end
+
+describe "Weightedpicker::lighten" do
+  before do
+    weights = { "A" => 1.0, "B" => 0.5, }
+    items = ["A", "B"]
+    File.open(TMP_FILE, "w") { |io| YAML.dump(weights, io) }
+    @wp01 = WeightedPicker.new(TMP_FILE, items)
+    @wp02 = Marshal.load(Marshal.dump(@wp01))
+    @wp02.weights = {}
+    FileUtils.rm NOT_EXIST_FILE if File.exist? NOT_EXIST_FILE
+  end
+
+  it "should lighten A" do
+    t = Marshal.load(Marshal.dump(@wp01))
+    t.lighten("A")
+    t.weights.should == { "A" => 1.0, "B" => 1.0 }
+    # 書き込みチェック
+    YAML.load_file(TMP_FILE).should == { "A" => 1.0, "B" => 1.0 }
+  end
+
+  it "should lighten B" do
+    t = Marshal.load(Marshal.dump(@wp01))
+    t.lighten("B")
+    t.weights.should == { "A" => 1.0, "B" => 0.25 }
+    # 書き込みチェック
+    YAML.load_file(TMP_FILE).should == { "A" => 1.0, "B" => 0.25 }
+  end
+
+  it "should raise error" do
+    t = Marshal.load(Marshal.dump(@wp01))
+    lambda{t.lighten("C")}.should raise_error(WeightedPicker::NotExistKeyError)
+  end
+
+  after do
+    FileUtils.rm TMP_FILE  if File.exist? TMP_FILE
+    FileUtils.rm NOT_EXIST_FILE if File.exist? NOT_EXIST_FILE
+  end
+
+end
+
+describe "Weightedpicker::adopt" do
+  before do
+    weights = { "A" => 1.0, "B" => 0.5, }
+    items = ["A", "B"]
+    File.open(TMP_FILE, "w") { |io| YAML.dump(weights, io) }
+    @wp01 = WeightedPicker.new(TMP_FILE, items)
+    @wp02 = Marshal.load(Marshal.dump(@wp01))
+    @wp02.weights = {}
+    FileUtils.rm NOT_EXIST_FILE if File.exist? NOT_EXIST_FILE
+  end
+
+  it do
+    @wp01.adopt?("A", 0.0  ).should be_true
+    @wp01.adopt?("A", 0.5  ).should be_true
+    @wp01.adopt?("A", 0.999).should be_true
+    @wp01.adopt?("A", 1.0  ).should be_false
+    @wp01.adopt?("B", 0.0  ).should be_true
+    @wp01.adopt?("B", 0.499).should be_true
+    @wp01.adopt?("B", 0.5  ).should be_false
+    @wp01.adopt?("B", 1.0  ).should be_false
+  end
+
+  after do
+    FileUtils.rm TMP_FILE  if File.exist? TMP_FILE
+    FileUtils.rm NOT_EXIST_FILE if File.exist? NOT_EXIST_FILE
+  end
+
+end
+
+describe "Weightedpicker::normalize_write" do
+  before do
+    weights = { "A" => 1.0, "B" => 0.5, }
+    items = ["A", "B"]
+    File.open(TMP_FILE, "w") { |io| YAML.dump(weights, io) }
+    @wp01 = WeightedPicker.new(TMP_FILE, items)
+    @wp02 = Marshal.load(Marshal.dump(@wp01))
+    @wp02.weights = {}
+    FileUtils.rm NOT_EXIST_FILE if File.exist? NOT_EXIST_FILE
+  end
+
+  it "should shrink write" do
+    t = Marshal.load(Marshal.dump(@wp01))
+    t.weights = { "A" => 2.0, "B" => 0.5, }
+    t.normalize_write
+    t.weights.should == { "A" => 1.0, "B" => 0.25}
+    # 書き込みチェック
+    YAML.load_file(TMP_FILE).should == { "A" => 1.0, "B" => 0.25}
+  end
+
+  it "should shrink write" do
+    t = Marshal.load(Marshal.dump(@wp01))
+    t.weights = { "A" => 0.5, "B" => 0.5, }
+    t.normalize_write
+    t.weights.should == { "A" => 1.0, "B" => 1.00}
+    # 書き込みチェック
+    YAML.load_file(TMP_FILE).should == { "A" => 1.0, "B" => 1.00}
+  end
+
+  it "should raise error" do
+    lambda{@wp02.normalize_write}.should raise_error (WeightedPicker::NoEntryError)
+  end
+
+  after do
+    FileUtils.rm TMP_FILE  if File.exist? TMP_FILE
+    FileUtils.rm NOT_EXIST_FILE if File.exist? NOT_EXIST_FILE
+  end
+
 end
